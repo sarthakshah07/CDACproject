2ed8cba9a75a6690067b5b51ca78353c95194377
sarthakshah07 <sarthakshah44993@gmail.com>
2022-10-05 02:17:08 +0530

    Add files via upload

---
+++ precache-manifest.f62629ccfc435b1d83505906a34cc251.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "e9a899a39dce5b7e632f",
+    "url": "/static/css/main.4622e7bc.chunk.css"
+  },
+  {
+    "revision": "e9a899a39dce5b7e632f",
+    "url": "/static/js/main.e9a899a3.chunk.js"
+  },
+  {
+    "revision": "22bc531f9c60224fd34b",
+    "url": "/static/js/1.22bc531f.chunk.js"
+  },
+  {
+    "revision": "46b85d7455bc962608bf",
+    "url": "/static/css/2.494a0d4d.chunk.css"
+  },
+  {
+    "revision": "46b85d7455bc962608bf",
+    "url": "/static/js/2.46b85d74.chunk.js"
+  },
+  {
+    "revision": "0fc329494775548ea42c",
+    "url": "/static/js/runtime~main.0fc32949.js"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "5fcf71fd1486806a865a4d8c338e22a4",
+    "url": "/index.html"
+  }
+];
